6063f39d20b0745ee929cc6a3c10148979c06d1a
Artem Leonov <arttemleonov@gmail.com>
2022-11-25 16:53:23 +0300

    init project

---
+++ src/main/java/com/javagentlemen/youaresenior/YouAreSeniorApplication.java
@@ -0,0 +1,13 @@
+package com.javagentlemen.youaresenior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouAreSeniorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YouAreSeniorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/javagentlemen/youaresenior/YouAreSeniorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javagentlemen.youaresenior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouAreSeniorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
