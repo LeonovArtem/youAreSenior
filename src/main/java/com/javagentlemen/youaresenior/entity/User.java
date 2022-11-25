b686dc0885820c6c15c391d825c3281b1db44978
Artem Leonov <a.leonov@dats.team>
2022-11-25 19:37:59 +0300

    add database

--- src/main/java/com/javagentlemen/youaresenior/entity/User.java
+++ src/main/java/com/javagentlemen/youaresenior/entity/User.java
@@ -1,10 +1,15 @@
 package com.javagentlemen.youaresenior.entity;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 }
